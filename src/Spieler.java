8eb02672d735d4cc680ca7c3ee577069ae99b49c
Programming viktor <106867471+okthisisepic@users.noreply.github.com>
2026-05-12 18:33:22 +0200

    added enumarator for Positions and ai question for random generated name which yes the whole code for that is a template from my old project

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Spieler {
     public String name;
     public int price;
-    enum PrefferredPosition {
-        Torwart
+    public enum PrefferredPosition {
+        Torwart,Innerverteidiger,Außenverteidiger,DefensiverMittelspieler,ZentralMittelfeldSpieler,RechterFlügelSpieler,LinkerFlügelSpieler,OffensiverMittelFeldSpieler;
     }
+
+    public PrefferredPosition getPosition() {
+        return Position;
+    }
+
+    public void setPosition(PrefferredPosition position) {
+        Position = position;
+    }
+
+
+    PrefferredPosition Position;
 
     public String getName() {
         return name;
@@ -19,5 +36,49 @@ public class Spieler {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+    public void gen() {
+        try {
+            String apiKey = "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";
+
+            URL url = new URL("https://api.groq.com/openai/v1/chat/completions");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Bearer " + apiKey);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+
+            String question = "Random Footballername OUTPUT JUST THE NAME NOTHING ELSE";
+
+            String json =
+                    "{"
+                            + "\"model\":\"llama-3.3-70b-versatile\","
+                            + "\"messages\":["
+                            + "{\"role\":\"user\",\"content\":\"" + question + "\"}"
+                            + "]"
+                            + "}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes());
+            os.flush();
+            os.close();
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream())
+            );
+
+            String output;
+
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
